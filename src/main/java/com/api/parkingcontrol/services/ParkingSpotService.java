07c938f082834645b5f2e108eb4281934d25bd02
limavit <limavit@gmail.com>
2023-01-24 16:16:17 -0300

    save service

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -3,13 +3,23 @@ package com.api.parkingcontrol.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
 	
 	@Autowired
 	ParkingSpotRepository parkingSpotRepository;
+	
+ 
+	@Transactional //garantir o rollback se algo de errado no banco
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		// TODO Auto-generated method stub
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
 	
 	
 
